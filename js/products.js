5963d9a14efd1cd44ff54d98f2d2b4d38ccb6e31
231101731005ulm <153398710+231101731005ulm@users.noreply.github.com>
2026-06-03 22:55:02 +0800

    Update products.js

--- js/products.js
+++ js/products.js
@@ -3,28 +3,28 @@ const products = [
         id: 1, 
         name: 'Original Mozza', 
         price: 15000, 
-        image: 'images/Original Mochi.jpeg', 
+        image: './images/Original Mochi.jpeg', 
         desc: 'Mochi krispi dengan isian mozzarella lumer yang gurih.' 
     },
     { 
         id: 2, 
         name: 'Chocolate Lava', 
         price: 18000, 
-        image: 'images/Choco Mochi.jpeg', 
+        image: './images/Choco Mochi.jpeg', 
         desc: 'Sensasi cokelat premium lumer yang melimpah di setiap gigitan.' 
     },
     { 
         id: 3, 
         name: 'Matcha Melt', 
         price: 18000, 
-        image: 'images/Matcha Mochi.jpeg', 
+        image: './images/Matcha Mochi.jpeg', 
         desc: 'Rasa matcha autentik yang creamy dan menenangkan.' 
     },
     { 
         id: 4, 
         name: 'Strawberry Cloud', 
         price: 17000, 
-        image: 'images/Strawberry Mochi.jpeg', 
+        image: './images/Strawberry Mochi.jpeg', 
         desc: 'Paduan manis asam stroberi segar dengan tekstur mochi yang lembut.' 
     }
 ];
