ed54c376cf3abb116a616f0d7f6efa414c559e42
231101731005ulm <153398710+231101731005ulm@users.noreply.github.com>
2026-06-11 22:29:24 +0800

    Update products.js

--- js/products.js
+++ js/products.js
@@ -7,7 +7,7 @@ const products = [
         originalPrice: 18000, 
         rating: 5.0,
         sold: 1250,
-        image: './images/Original Mochi.jpeg', 
+        image: './images/Box Original Mochi.jpeg', 
         desc: 'Mochi krispi dengan isian mozzarella lumer yang gurih.' 
     },
     { 
@@ -17,17 +17,17 @@ const products = [
         originalPrice: 20000,
         rating: 4.9,
         sold: 980,
-        image: './images/Choco Mochi.jpeg', 
+        image: './images/Box Choco Mochi.jpeg', 
         desc: 'Sensasi cokelat premium lumer yang melimpah di setiap gigitan.' 
     },
     { 
         id: 3, 
         name: 'Matcha Melt', 
         price: 18000, 
-        originalPrice: null, // Kosongkan jika tidak ada diskon
+        originalPrice:, 
         rating: 4.8,
         sold: 850,
-        image: './images/Matcha Mochi.jpeg', 
+        image: './images/Box Matcha Mochi.jpeg', 
         desc: 'Rasa matcha autentik yang creamy dan menenangkan.' 
     },
     { 
@@ -37,7 +37,7 @@ const products = [
         originalPrice: 19000,
         rating: 4.9,
         sold: 620,
-        image: './images/Strawberry Mochi.jpeg', 
+        image: './images/Box Strawberry Mochi.jpeg', 
         desc: 'Paduan manis asam stroberi segar dengan tekstur mochi yang lembut.' 
     }
 ];
